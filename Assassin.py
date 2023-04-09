2fd0f982b8f23b7ba46dd5185bc4b3c5968376fa
Noah Alderton <noahlouisalderton@gmail.com>
2023-04-09 03:41:30 -0700

    Improve UI

--- Assassin.py
+++ Assassin.py
@@ -7,47 +7,63 @@ from tkinter.filedialog import askopenfilename
 import os
 import random
 import copy
+import re
+
 seniors_array = []
 wb = None
 ws = None
+fromAddress = ""
+password = ""
 
-
-
-#Tkinter Window Setup
+# Tkinter Window Setup
 main = Tk()
 main.title("Senior Assassin")
-main.geometry("420x300")
+main.geometry("530x340")
 
-#Excel Sheet Label
-excel_sheet_label = Label(main, text = "File Name:")
-excel_sheet_label.place(x = 5, y = 50)
+header_frame = Frame(main)
+header_frame.grid(row = 0, column = 1, sticky = W, pady = 2)
 
-#Excel Sheet Name Label
-excel_sheet_name_label = Label(main, text = "No File Initialized")
-excel_sheet_name_label.place(x = 80, y = 50)
+# Excel Sheet Label
+excel_sheet_label = Label(header_frame, text = "File Name:")
+excel_sheet_label.grid(row = 0, column = 0, sticky = W, pady = 2)
 
-#Status Label
+# Excel Sheet Name Label
+excel_sheet_name_label = Label(header_frame, text = "No File Initialized")
+excel_sheet_name_label.grid(row = 0, column = 1, sticky = W, pady = 2)
+
+# Status Label
 status_label = Label(main, text = "")
-status_label.pack(side = TOP)
+status_label.grid(row = 1, column = 1, sticky = W, pady = 2)
 
-#Assignment Scroll Table
-frame = Frame(main)
-frame.place(x = 5, y = 80)
-assassin_list = Listbox(frame, width = 43)
+from_address_label = Label(main, text = "From Address")
+fromAddress = Entry(main)
+from_address_label.grid(row = 2, column = 0, sticky = W, pady = 2)
+fromAddress.grid(row = 2, column = 1, sticky="nsew", pady = 2)
+
+password_label = Label(main, text = "Password")
+password = Entry(main, show = "*")
+password_label.grid(row = 3, column = 0, sticky = W, pady = 2)
+password.grid(row = 3, column = 1, sticky="nsew", pady = 2)
+
+# Assignment Scroll Table
+assignment_frame = Frame(main)
+assignment_frame.grid(row = 4, column = 1, sticky="nsew", pady = 2)
+
+assassin_list = Listbox(assignment_frame, width = 43)
 assassin_list.pack(side = "left", fill = "y")
-scrollbar = Scrollbar(frame, orient="vertical")
+scrollbar = Scrollbar(assignment_frame, orient="vertical")
 scrollbar.pack(side = RIGHT, fill=Y)
 scrollbar.config(command = assassin_list.yview)
-assassin_list.config(yscrollcommand=scrollbar.set)
+assassin_list.config(yscrollcommand = scrollbar.set)
 
 assassin_list.insert(END, "There is currently no data")
 
 
-#Function for closing window
+# Function for closing window
 def close_window():
     main.destroy()
 
-#openpyxl Workbook Setup
+# openpyxl Workbook Setup
 def workbook_setup():
     global seniors_array
     global wb
@@ -91,66 +107,83 @@ def workbook_setup():
     print(seniors_array)
 
 def save():
-    #Create New Workbook
     master_wb = openpyxl.Workbook()
     master_ws = master_wb.active
-    master_ws.cell(row=1, column=1).value = "Assassin First"
-    master_ws.cell(row=1, column=2).value = "Assassin Last"
-    master_ws.cell(row=1, column=3).value = "Assassin Email"
-    master_ws.cell(row=1, column=4).value = "Target First"
-    master_ws.cell(row=1, column=5).value = "Target Last"
+    master_ws.cell(row = 1, column = 1).value = "Assassin First"
+    master_ws.cell(row = 1, column = 2).value = "Assassin Last"
+    master_ws.cell(row = 1, column = 3).value = "Assassin Email"
+    master_ws.cell(row = 1, column = 4).value = "Target First"
+    master_ws.cell(row = 1, column = 5).value = "Target Last"
     currentDirectory = os.getcwd()
     for current in range(0, len(seniors_array)):
         row = current + 2
-        master_ws.cell(row=row, column=1).value = seniors_array[current][0]
-        master_ws.cell(row=row, column=2).value = seniors_array[current][1]
-        master_ws.cell(row=row, column=3).value = seniors_array[current][2]
-        master_ws.cell(row=row, column=4).value = seniors_array[current][3]
-        master_ws.cell(row=row, column=5).value = seniors_array[current][4]
+        master_ws.cell(row = row, column = 1).value = seniors_array[current][0]
+        master_ws.cell(row = row, column = 2).value = seniors_array[current][1]
+        master_ws.cell(row = row, column = 3).value = seniors_array[current][2]
+        master_ws.cell(row = row, column = 4).value = seniors_array[current][3]
+        master_ws.cell(row = row, column = 5).value = seniors_array[current][4]
         master_wb.save(str(currentDirectory) + "/Master_List.xlsx")
 
 def email():
     if wb == None or ws == None:
         status_label.config(text="Error: You Must Initiate a File")
 
-    else:
-        for current in range(0, len(seniors_array)):
+    if not isEmailValid(fromAddress):
+        status_label.config(text="Error: Invalid Formatted From Email Address")
 
-            #To, From, Message
-            fromaddr = " " #Enter a Gmail email address in the string
+    if password == "":
+        status_label.config(text="Error: Password empty")
+
+
+    else:
+        # SMTP Settings
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+
+        try:
+            server.login(fromAddress, password)
+        except:
+            status_label.config(text="Error: Unable to authenticate.  Double check your from email address and password.")
+
+        for current in range(0, len(seniors_array)):
+            # To, From, Message
             toaddr = seniors_array[current][2]
             msg = MIMEMultipart()
-            msg['From'] = fromaddr
+            msg['From'] = fromAddress
             msg['To'] = toaddr
             msg['Subject'] = "Senior Assassin"
 
-            #Message Body
-            body = seniors_array[current][0] + " " + seniors_array[current][1]+ ",\n" + "You need to assassinate " + seniors_array[current][3] + " " + seniors_array[current][4]
+            # Message Body
+            body = seniors_array[current][0] + " " + seniors_array[current][1]+ ",\n" + "You have been assigned  " + seniors_array[current][3] + " " + seniors_array[current][4]
             msg.attach(MIMEText(body, 'plain'))
 
-            #SMTP Settings
-            server = smtplib.SMTP('smtp.gmail.com', 587)
-            server.starttls()
-            server.login(fromaddr, " " ) #Enter the password for the Gmail account in the string
+
             text = msg.as_string()
-            server.sendmail(fromaddr, toaddr, text)
+            server.sendmail(fromAddress, toaddr, text)
             server.quit()
 
+# Determine if email is valid
+def isEmailValid(email):
+    regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+    return re.fullmatch(regex, email)
 
-#Initiate button
-init_button = Button(main, text="Initiate Workbook", command = workbook_setup)
-init_button.place(x = 70, y = 265)
+buttons = Frame(main)
+buttons.grid(row = 5, column = 1, sticky=E)
 
-#Email Button
-email_button = Button(main, text="Email", command = email)
-email_button.place(x = 210, y = 265)
+# Initiate button
+init_button = Button(buttons, text = "Initiate Workbook", command = workbook_setup)
+init_button.grid(row = 0, column = 1, padx = 2)
 
-#Save Button
-save_button = Button(main, text="Save", command = save)
-save_button.place(x = 270, y = 265)
+# Email Button
+email_button = Button(buttons, text = "Email", command = email)
+email_button.grid(row = 0, column = 2, padx = 2)
 
-#Quit Button
-quit_button = Button(main, text="Quit", command = close_window)
-quit_button.place(x = 340, y = 265)
+# Save Button
+save_button = Button(buttons, text = "Save", command = save)
+save_button.grid(row = 0, column = 3, padx = 2)
+
+# Quit Button
+quit_button = Button(buttons, text = "Quit", command = close_window)
+quit_button.grid(row = 0, column = 4, padx = 2)
 
 main.mainloop()
