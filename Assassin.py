bdff818c79a5a613c641d3dcbd79b5f880b08d8d
Noah Alderton <noahlouisalderton@gmail.com>
2023-04-09 06:26:20 -0700

    Use CSVs & Senior object

--- Assassin.py
+++ Assassin.py
@@ -1,18 +1,48 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-import openpyxl
+import csv
 from tkinter import *
 from tkinter.filedialog import askopenfilename
 import os
 import random
-import copy
 import re
 
+class Senior:
+    def __init__(self, first_name: str, last_name: str, email: str):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+
+    def setTarget(self, first_name: str, last_name: str):
+        self.target_first_name = first_name
+        self.target_last_name = last_name
+
+    def isTarget(self, senior: object) -> bool:
+        try:
+            return self.target_first_name == senior.first_name and self.target_last_name == senior.last_name
+        except AttributeError:
+            return False
+
+    def getCsvRow(self) -> list:
+        return [
+            self.first_name,
+            self.last_name,
+            self.email,
+            self.target_first_name,
+            self.target_last_name
+        ]
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, Senior):
+            return self.email == other.email
+    
+    def __str__(self):
+        return self.first_name + " " + self.last_name + " - " + self.email
+
+
 seniors_array = []
-wb = None
-ws = None
-fromAddress = ""
+from_address = ""
 password = ""
 
 # Tkinter Window Setup
@@ -24,24 +54,24 @@ header_frame = Frame(main)
 header_frame.grid(row = 0, column = 1, sticky = W, pady = 2)
 
 # Excel Sheet Label
-excel_sheet_label = Label(header_frame, text = "File Name:")
-excel_sheet_label.grid(row = 0, column = 0, sticky = W, pady = 2)
+file_label = Label(header_frame, text = "File Name:")
+file_label.grid(row = 0, column = 0, sticky = W, pady = 2)
 
 # Excel Sheet Name Label
-excel_sheet_name_label = Label(header_frame, text = "No File Initialized")
-excel_sheet_name_label.grid(row = 0, column = 1, sticky = W, pady = 2)
+file_name_label = Label(header_frame, text = "No File Initialized")
+file_name_label.grid(row = 0, column = 1, sticky = W, pady = 2)
 
 # Status Label
 status_label = Label(main, text = "")
 status_label.grid(row = 1, column = 1, sticky = W, pady = 2)
 
 from_address_label = Label(main, text = "From Address")
-fromAddress = Entry(main)
+from_address = Entry(main)
 from_address_label.grid(row = 2, column = 0, sticky = W, pady = 2)
-fromAddress.grid(row = 2, column = 1, sticky="nsew", pady = 2)
+from_address.grid(row = 2, column = 1, sticky="nsew", pady = 2)
 
 password_label = Label(main, text = "Password")
-password = Entry(main, show = "*")
+password = Entry(main, show = "*", width=43)
 password_label.grid(row = 3, column = 0, sticky = W, pady = 2)
 password.grid(row = 3, column = 1, sticky="nsew", pady = 2)
 
@@ -63,77 +93,63 @@ assassin_list.insert(END, "There is currently no data")
 def close_window():
     main.destroy()
 
-# openpyxl Workbook Setup
-def workbook_setup():
+def csv_setup():
     global seniors_array
-    global wb
-    global ws
+    seniors_array.clear()
     file_path = askopenfilename()
     extension = os.path.splitext(file_path)[1]
     basename = os.path.basename(file_path)
-    if extension ==".xlsx":
-        wb = openpyxl.load_workbook(file_path)
-        ws = wb.worksheets[0]
-        if ws.cell(row = 1, column = 2).value == "First Name":
-            status_label.config(text="Workbook Successfully Opened")
-            excel_sheet_name_label.config(text=basename)
-            assassin_list.delete(0, END)
-            for row in range(2, ws.max_row + 1):
-                current_person = []
-                for column in range(2, 5):
-                    current_person.append(ws.cell(row=row, column=column).value)
-                seniors_array.append(current_person)
-            selection_pool = copy.deepcopy(seniors_array)
+    if extension == ".csv":
+        with open(file_path, 'r', newline='') as file:
+            reader = csv.DictReader(file, delimiter=',',)
+            status_label.config(text="CSV Successfully Opened")
+            file_name_label.config(text=basename)
+            for row in reader:
+                try:
+                    current_person = Senior(
+                        row["First Name"],
+                        row["Last Name"],
+                        row["Email Address"]
+                    )
+                    seniors_array.append(current_person)
+                except KeyError:
+                    status_label.config(text="Error: Not a Valid File")
 
-            for current in range(0, len(seniors_array)):
+            selection_pool = list(range(len(seniors_array)))
+            assassin_list.delete(0, END) # Clear ListBox
+
+            for current in seniors_array:
                 random_pick = random.choice(selection_pool)
-                while seniors_array[current] == random_pick:
+                while current == seniors_array[random_pick] or seniors_array[random_pick].isTarget(current):
                     random_pick = random.choice(selection_pool)
-                seniors_array[current].append(random_pick[0])
-                seniors_array[current].append(random_pick[1])
+                current.setTarget(seniors_array[random_pick].first_name, seniors_array[random_pick].last_name)
                 selection_pool.remove(random_pick)
 
-                #Update Listbox
-                assassin_list.insert(END, str(seniors_array[current][0]) + " " + str(seniors_array[current][1]) + " - " + str(seniors_array[current][3]) + " " + str(seniors_array[current][4]) + " - " + str(seniors_array[current][2]))
+                # Update Listbox
+                assassin_list.insert(END, str(current.first_name) + " " + str(current.last_name) + " - " + str(current.target_first_name) + " " + str(current.target_last_name) + " - " + str(current.email))
 
-        else:
-            status_label.config(text="Error: Not a Valid File")
-            wb = None
-            ws = None
     else:
         status_label.config(text="Error: Not a Valid File")
-        wb = None
-        ws = None
-    print(seniors_array)
 
 def save():
-    master_wb = openpyxl.Workbook()
-    master_ws = master_wb.active
-    master_ws.cell(row = 1, column = 1).value = "Assassin First"
-    master_ws.cell(row = 1, column = 2).value = "Assassin Last"
-    master_ws.cell(row = 1, column = 3).value = "Assassin Email"
-    master_ws.cell(row = 1, column = 4).value = "Target First"
-    master_ws.cell(row = 1, column = 5).value = "Target Last"
-    currentDirectory = os.getcwd()
-    for current in range(0, len(seniors_array)):
-        row = current + 2
-        master_ws.cell(row = row, column = 1).value = seniors_array[current][0]
-        master_ws.cell(row = row, column = 2).value = seniors_array[current][1]
-        master_ws.cell(row = row, column = 3).value = seniors_array[current][2]
-        master_ws.cell(row = row, column = 4).value = seniors_array[current][3]
-        master_ws.cell(row = row, column = 5).value = seniors_array[current][4]
-        master_wb.save(str(currentDirectory) + "/Master_List.xlsx")
+    with open('targetList.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        fields = ["First Name", "Last Name", "Email", "Target First Name", "Target Last Name"]
+        writer.writerow(fields)
+        for senior in seniors_array:
+            writer.writerow(senior.getCsvRow())
+
+    status_label.config(text="targetList.csv Saved!")
 
 def email():
-    if wb == None or ws == None:
+    if not seniors_array:
         status_label.config(text="Error: You Must Initiate a File")
 
-    if not isEmailValid(fromAddress):
+    if not isEmailValid(from_address.get()):
         status_label.config(text="Error: Invalid Formatted From Email Address")
 
     if password == "":
         status_label.config(text="Error: Password empty")
-
 
     else:
         # SMTP Settings
@@ -141,25 +157,23 @@ def email():
         server.starttls()
 
         try:
-            server.login(fromAddress, password)
+            server.login(from_address.get(), password.get())
         except:
             status_label.config(text="Error: Unable to authenticate.  Double check your from email address and password.")
 
-        for current in range(0, len(seniors_array)):
-            # To, From, Message
-            toaddr = seniors_array[current][2]
+        for current in seniors_array:
+            to_address = current.email
             msg = MIMEMultipart()
-            msg['From'] = fromAddress
-            msg['To'] = toaddr
+            msg['From'] = from_address.get()
+            msg['To'] = to_address
             msg['Subject'] = "Senior Assassin"
 
             # Message Body
-            body = seniors_array[current][0] + " " + seniors_array[current][1]+ ",\n" + "You have been assigned  " + seniors_array[current][3] + " " + seniors_array[current][4]
+            body = current.first_name + " " + current.last_name + ",\n" + "You have been assigned  " + current.target_first_name + " " + current.target_last_name
             msg.attach(MIMEText(body, 'plain'))
 
-
             text = msg.as_string()
-            server.sendmail(fromAddress, toaddr, text)
+            server.sendmail(from_address.get(), to_address, text)
             server.quit()
 
 # Determine if email is valid
@@ -171,7 +185,7 @@ buttons = Frame(main)
 buttons.grid(row = 5, column = 1, sticky=E)
 
 # Initiate button
-init_button = Button(buttons, text = "Initiate Workbook", command = workbook_setup)
+init_button = Button(buttons, text = "Initiate CSV", command = csv_setup)
 init_button.grid(row = 0, column = 1, padx = 2)
 
 # Email Button
